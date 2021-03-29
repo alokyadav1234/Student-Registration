b8406690d52badc8beeecb39bab7a0acb51e60de
alokyadav1234 <1234.alok@gmail.com>
2021-03-29 17:17:14 +0530

    Add files via upload

---
+++ server/routes/Student.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+
+router.get('/', (req, res) => {
+    
+})
+
+
+
+
+
+module.exports = router;
